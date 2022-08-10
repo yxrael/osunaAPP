74b6d6a182c44e2ed5c7e8c0294c824d880673a4
yxrael <yxrael@gmail.com>
2022-08-10 02:19:25 +0200

    config maps ios

--- ios/asemproAPP/AppDelegate.mm
+++ ios/asemproAPP/AppDelegate.mm
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -31,6 +33,8 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  GMSServices provideAPIKey:@"AIzaSyDweu3vEu5lC7PpwViodKG7cMNEgaow1F8"];
+
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
